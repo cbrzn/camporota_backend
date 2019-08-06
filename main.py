e778b432d9bcfa0705753a27f6c166c0df0d3144
cbrzn <cesarbrazon10@gmail.com>
2019-08-06 15:17:20 +0200

    setting up static port

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from app import create_app
 import os
 if __name__ == '__main__':
     server = create_app()
-    port = int(os.environ.get("BACKEND_PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     server.run(host='0.0.0.0', port=port)
