cd9d70485144dec3af89a576e89434d8d50b5d9b
cbrzn <cesarbrazon10@gmail.com>
2019-09-07 15:06:06 +0200

    modifying app variable

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from os import environ
 
-import api
+import api as app
 import api.db as database
-
-app = api.server.start.app 
 
 if __name__ == '__main__':
     port = int(environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.server.start.run(host='0.0.0.0', port=port)
